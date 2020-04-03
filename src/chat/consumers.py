291e1bae79cf1771f4340b4dd1b09f8e73fe5c43
anant <anantdevdubey@hotmail.com>
2020-04-03 21:45:11 +0530

    echoing and displaying the data sent by sockets

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -28,6 +28,23 @@ class ChatConsumer(AsyncConsumer):
 
     async def websocket_receive(self, event):
         print('received', event)
+        front_text = event.get('text', None)
+        if front_text is not None:
+            loaded_dict_data = json.loads(front_text)
+            msg = loaded_dict_data.get('message')
+            print(msg)
+            user = self.scope['user']
+            username = 'default'
+            if user.is_authenticated:
+                username = user.username
+            my_response = {
+                'message': msg,
+                'username': username
+            }
+            await self.send({
+                'type': 'websocket.send',
+                'text': json.dumps(my_response)
+            })
 
     async def websocket_disconnect(self, event):
         print('disconnected', event)
